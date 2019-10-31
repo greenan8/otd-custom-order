4be964dae70df9de865947210becf2ade95f4b7f
Andrew Greenan <andrewbgreenan@gmail.com>
2019-10-30 20:48:40 -0400

    airtable costs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//retrieving different clothing options from airtable base
+//============================= airtable =============================
 require('dotenv').config()
 var Airtable = require('airtable');
 const clothingBase = Airtable.base('appmKG59YjZb91BWI');
@@ -23,7 +23,7 @@ function getAirtableData(){
             allColors = records;
     }); 
 
-    clothingBase('Name Print').select({
+    clothingBase('Logo Print').select({
         view: 'Grid view'
     }).firstPage(function(err, records) {
         if (err) { console.error(err); return; }
@@ -49,7 +49,6 @@ function getAirtableData(){
             logoPrintOptions = record.get('Logo Print Options');
             logoPrintOptions && logoPrintOptions.forEach(function(option){
                 clothingRecords[record.id]['logoPrintOptions'].push(option); 
-                console.log(option);
             });
 
             textPrintOptions = record.get('Text Print Options');
@@ -61,10 +60,48 @@ function getAirtableData(){
 
 };
 
+var margins={}, printCost, printExtraCosts={}, stitchCost, stitchExtaCosts={};
+
+
+function getAirtableCostData(){  
+    
+    costBase('Margins').select({
+        view: 'Grid view'
+    }).firstPage(function(err, records) {
+        if (err) { console.error(err); return; }
+        records.forEach(function(record){
+           margins[record.get("Name")] = record.get("Margin");
+        }); 
+    }); 
+
+    costBase('Printing Additional Charges').select({
+        view: 'Grid view'
+    }).firstPage(function(err, records) {
+        if (err) { console.error(err); return; }
+        records.forEach(function(record){
+           margins[record.get("Name")] = record.get("Price");
+        }); 
+    }); 
+
+    costBase('Stitching Additional Charges').select({
+        view: 'Grid view'
+    }).firstPage(function(err, records) {
+        if (err) { console.error(err); return; }
+        records.forEach(function(record){
+           margins[record.get("Name")] = record.get("Price");
+        }); 
+    }); 
+
+    //todo: printcosts and stitchcost
+
+};
+
+//calling both to retrieve data when server loadas
 getAirtableData()
+getAirtableCostData()
 
 
-
+//============================= express setup =============================
 //setting up app (aka getting express, view engine, and static files good to go)
 var express = require('express');
 var app = express();
@@ -72,6 +109,7 @@ app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
 
 
+//============================= All paths =============================
 //Load webapp
 app.get('/', (req, res) => {
     res.render('index', {allRecords, clothingRecords, allColors, allPrintOptions});
@@ -107,7 +145,7 @@ app.get('/allPrintOptions', (req, res) => {
     res.send(allPrintOptions);
 });
 
-//Mailing submitted form to otd email
+//============================= nodemailer =============================
 
 
 
